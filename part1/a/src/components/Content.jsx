41a33a32e01c820f6bce16c9962a5b0a7ea4c75e
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-10-17 19:46:09 +0530

    completed Exercise 1.3

--- part1/a/src/components/Content.jsx
+++ part1/a/src/components/Content.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-const Part = ({ part, exercises }) => {
+const Part = (props) => {
+  console.log(props)
   return (
     <div>
-      {part} {exercises}
+      {props.part.name} {props.part.exercises}
     </div>
   );
 };
 
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1}  />
+      <Part part={props.part2}  />
+      <Part part={props.part3}  />
     </div>
   );
 };
