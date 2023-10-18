5f9df52797239a695760b4e712a48e42cf87223c
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-10-18 19:51:18 +0530

    completed Exercise 1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,17 @@ const Header = ({ title }) => {
   return <h1>{title}</h1>;
 };
 
+const Button = ({ text, color, onClick }) => {
+  const buttonStyle = {
+    backgroundColor: color,
+  };
+
+  return (
+    <button style={buttonStyle} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
 function App() {
   // save clicks of each button to its own state
@@ -11,29 +22,35 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodButton = () => {
-    setGood(good + 1);
+  const handleButtonClick = (feedback) => {
+    if (feedback === "good") {
+      setGood(good + 1);
+    } else if (feedback === "neutral") {
+      setNeutral(neutral + 1);
+    } else if (feedback === "bad") {
+      setBad(bad + 1);
+    }
   };
+  const all = good + neutral + bad;
 
-  const handleNeutralButton = () => {
-    setNeutral(neutral + 1);
-  };
-  const handleBadButton = () => {
-    setBad(bad + 1);
-  };
+   // Calculate the average, considering "neutral" as a value of 0
+   const average = (good - bad) / all || 0;
 
+    // Calculate the percentage of "good" reviews
+  const positive = (good / all) * 100 || 0;
   return (
     <>
       <Header title="give feedback" />
-      <button onClick={handleGoodButton}>Good</button>{" "}
-      <button onClick={handleNeutralButton}>neutral</button>{" "}
-      <button onClick={handleBadButton}>bad</button>
+      <Button text="Good" color="green" onClick={() => handleButtonClick("good")}/>{" "}
+      <Button  text="Neutral" color="gray" onClick={() => handleButtonClick("neutral")}/> {" "}
+      <Button text="Bad" color="red" onClick={() => handleButtonClick("bad")}/>
       <Header title="statistics" />
-      good {good}
-      <br />
-      neutral {neutral}
-      <br />
-      bad {bad}
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average.toFixed(2)}</p>
+      <p>positive {positive.toFixed(2)}%</p>
     </>
   );
 }
