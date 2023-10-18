8a6f924870dd1a8824cb78d1f071cb782acd7d5b
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-10-19 05:21:14 +0530

    completed Exercise 1.9: unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,14 +4,13 @@ import Button from "./componets/Button";
 
 import Header from "./componets/Header";
 
-import Statistics  from "./componets/Statistics";
-
+import Statistics from "./componets/Statistics";
 
 function App() {
- 
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [feedbackGiven, setFeedbackGiven] = useState(false);
 
   const handleButtonClick = (feedback) => {
     if (feedback === "good") {
@@ -21,17 +20,30 @@ function App() {
     } else if (feedback === "bad") {
       setBad(bad + 1);
     }
+
+    setFeedbackGiven(true);
   };
 
- 
   return (
     <>
       <Header title="give feedback" />
-      <Button text="Good" color="green" onClick={() => handleButtonClick("good")}/>{" "}
-      <Button  text="Neutral" color="gray" onClick={() => handleButtonClick("neutral")}/> {" "}
-      <Button text="Bad" color="red" onClick={() => handleButtonClick("bad")}/>
+      <Button
+        text="Good"
+        color="green"
+        onClick={() => handleButtonClick("good")}
+      />{" "}
+      <Button
+        text="Neutral"
+        color="gray"
+        onClick={() => handleButtonClick("neutral")}
+      />{" "}
+      <Button text="Bad" color="red" onClick={() => handleButtonClick("bad")} />
       <Header title="statistics" />
-      <Statistics  good={good} neutral={neutral} bad={bad} />
+      {feedbackGiven ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <h2>No feedback Given</h2>
+      )}
     </>
   );
 }
