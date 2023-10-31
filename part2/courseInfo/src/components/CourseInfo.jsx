5fa2cc84add093d7b6c0f7d0ddb1e0ac07665bd0
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-10-31 13:40:42 +0530

    Completed 2.3*: Course information step8 and 2.4*: Course information step9

--- part2/courseInfo/src/components/CourseInfo.jsx
+++ part2/courseInfo/src/components/CourseInfo.jsx
@@ -1,35 +1,36 @@
+import React from "react";
+import "./CourseInfo.css";
 
-import React from 'react'
-import "./CourseInfo.css"
-const Part = ({part})=>{
-  console.log(part)
-return(
-  <div>
-    {part.name} {part.exercises}
-  </div>
-)
-}
+const Part = ({ part }) => {
+  console.log(part);
+  return (
+    <div>
+      {part.name} {part.exercises}
+    </div>
+  );
+};
 
-const CourseInfo = ({courses}) => {
-  console.log(courses)
+const CourseInfo = ({ courses }) => {
+  console.log(courses);
 
   return (
     <>
-    <h1>CourseInfo</h1>
-    {courses.map((course) =>(
-      <div key={course.id}>
-      <h2>{course.name}</h2>
-      {course.parts.map((part)=>(
-        <Part key={part.id} part = {part}/>
+      <h1>CourseInfo</h1>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <h2>{course.name}</h2>
+          {course.parts.map((part) => (
+            <Part key={part.id} part={part} />
+          ))}
+          <p>
+            total exercises for {course.name} :{" "}
+            {course.parts.reduce((total, part) => total + part.exercises, 0)}{" "}
+            exercises.
+          </p>
+        </div>
       ))}
-      <p>total exercises  for {course.name} : {course.parts.reduce((total, part) => total + part.exercises , 0)} exercises.</p>
-      </div>
-    ))
-      
-    }
     </>
-    
-  )
-}
+  );
+};
 
 export default CourseInfo;
