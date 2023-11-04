6137333f3a7083f38fb5efe7f01972286ce1b619
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-11-04 19:38:39 +0530

    Completed Exercise-:2.7: The Phonebook Step2

--- part2/PhoneBook/src/App.jsx
+++ part2/PhoneBook/src/App.jsx
@@ -1,36 +1,45 @@
-import { useState } from "react"
-
-
+import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 
-  const [newName , setNewName] = useState("");
- 
-  const handleAdd= (event) => {
- event.preventDefault();
-    const personObject= {name:newName}
-    setPersons( persons.concat(personObject))
-   setNewName("")
+  const [newName, setNewName] = useState("");
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    const personObject = { name: newName };
+
+    const isPresent = persons.some(
+      (person) => person.name === personObject.name
+    );
+    isPresent
+      ? alert(`${newName} is already present in the PhoneBook.`)
+      : setPersons(persons.concat(personObject));
+    setNewName("");
   };
   const handleSetNewName = (event) => {
-  
-console.log(event.target.value);
-setNewName(event.target.value);
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
   return (
     <>
       <h1>PhoneBook</h1>
       <form>
-      <p>name:<input value={newName} onChange={handleSetNewName} /></p>
-      <button type="submit" onClick={handleAdd}>add</button>
+        <p>
+          name:
+          <input value={newName} onChange={handleSetNewName} />
+        </p>
+        <button type="submit" onClick={handleAdd}>
+          add
+        </button>
       </form>
       <h1>Numbers</h1>
-      {persons.map(person =>(<div key={person.id}>{person.name}</div>))}
-      
+      {persons.map((person) => (
+        <div key={person.id}>{person.name}</div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
