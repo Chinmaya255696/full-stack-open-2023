d05f48df2b7ea2128a9e4e33dfbb77765780b2f7
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2024-04-17 01:23:22 +0530

    EX_3.6: Phonebook backend step 6

--- part3/PhoneBook_Backend/index.js
+++ part3/PhoneBook_Backend/index.js
@@ -33,40 +33,48 @@ app.delete("/api/persons/:id", (req, res) => {
   const personIndex = data.findIndex((person) => {
     return person.id === id;
   });
-  console.log(personIndex)
- if (personIndex !== -1) {
+  console.log(personIndex);
+  if (personIndex !== -1) {
     data.splice(personIndex, 1);
-   // const data1 = data.filter((human) => human.id !== id);
+    // const data1 = data.filter((human) => human.id !== id);
     console.log(data);
-    res.status(204).send(`<h3>The Person with id ${id} has been deleted</h3>`)
-  }else{
+    res.status(204).send(`<h3>The Person with id ${id} has been deleted</h3>`);
+  } else {
     res.status(404).send("<h4>The Person does not exist in our Database</h4>");
   }
-  
-})
-let generateId = () =>{
-    const maxId = data.length>0 ? Math.max(...data.map(person => person.id)) : 0
-    return maxId+1
-}
-app.post("/api/persons", (req, res) => {
-    const { name, number } = req.body;
-
-    if (!name || !number) {
-        return res.status(400).send("<h4>Person data is missing</h4>");
-    }
-
-    const person = {
-        name: name, 
-        number: number,
-        id: generateId(),
-    };
-
-    data.push(person);
-
-    return res.json(data);
 });
+let generateId = () => {
+  const maxId =
+    data.length > 0 ? Math.max(...data.map((person) => person.id)) : 0;
+  return maxId + 1;
+};
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
 
+  if (!name || !number) {
+    return res
+      .status(400)
+      .send("<h4>Person both name or number is missing</h4>");
+  }
 
+  exsistingPerson = data.find((person) => person.name === name);
+
+  if (exsistingPerson) {
+    return res
+      .status(400)
+      .send(`<h4>The Person named ${name} already exists in our Database</h4>`);
+  }
+
+  const person = {
+    name: name,
+    number: number,
+    id: generateId(),
+  };
+
+  data.push(person);
+
+  return res.json(data);
+});
 
 const PORT = 3001;
 
