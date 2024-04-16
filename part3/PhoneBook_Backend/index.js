1961b834b1a3b995af54a6bd7be8b3cfbf632c2a
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2024-04-16 23:56:48 +0530

    EX_3.3: Phonebook backend step 3

--- part3/PhoneBook_Backend/index.js
+++ part3/PhoneBook_Backend/index.js
@@ -3,21 +3,32 @@ const app = express();
 const data = require("./Data");
 app.use(express.json());
 
+app.get("/api/persons", (req, res) => {
+  res.json(data);
+});
 
+app.get("/info", (req, res) => {
+  let info = `Phoneboook has info of ${data.length} People <br/>
+    ${Date()} `;
+  res.send(info);
+});
 
-app.get('/api/persons',(req, res)=>{
-    res.json(data);
-})
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
 
+  const person = data.find((person) => {
+    return person.id === id;
+  });
 
-app.get('/info',(req, res)=>{
-    let info = `Phoneboook has info of ${data.length} People <br/>
-    ${Date()} `
-    res.send(info);
-})
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send("<h4>The Person does not exist in our Database</h4>");
+  }
+});
 
 const PORT = 3001;
 
-app.listen(PORT, (req, res)=>{
-console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Server running on port ${PORT}`);
+});
